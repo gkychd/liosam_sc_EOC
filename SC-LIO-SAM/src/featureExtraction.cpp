4225fe6bf2ee5083e221d3374487762b87b44779
gky <352191543@qq.com>
2022-10-10 00:45:14 +0800

    add calculate plane normal vector

--- SC-LIO-SAM/src/featureExtraction.cpp
+++ SC-LIO-SAM/src/featureExtraction.cpp
@@ -1,6 +1,19 @@
 #include "utility.h"
 #include "lio_sam/cloud_info.h"
 
+#include <pcl/common/transforms.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/search/impl/search.hpp>
+#include <pcl/filters/impl/plane_clipper3D.hpp>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/sample_consensus/ransac.h>
+#include <pcl/sample_consensus/sac_model_plane.h>
+
+#include <lio_sam/FloorCoeffs.h>
+
+#include <boost/optional.hpp>
+#include <pcl_ros/point_cloud.h>
+#include <pluginlib/class_list_macros.h>
 
 struct smoothness_t{ 
     float value;
@@ -23,6 +36,10 @@ public:
     ros::Publisher pubLaserCloudInfo;
     ros::Publisher pubCornerPoints;
     ros::Publisher pubSurfacePoints;
+
+    ros::Publisher floor_filtered_pub;
+    ros::Publisher floor_points_pub;
+    ros::Publisher floor_pub;
 
     pcl::PointCloud<PointType>::Ptr extractedCloud;
     pcl::PointCloud<PointType>::Ptr cornerCloud;
@@ -47,6 +64,10 @@ public:
         pubLaserCloudInfo = nh.advertise<lio_sam::cloud_info> ("lio_sam/feature/cloud_info", 1);
         pubCornerPoints = nh.advertise<sensor_msgs::PointCloud2>("lio_sam/feature/cloud_corner", 1);
         pubSurfacePoints = nh.advertise<sensor_msgs::PointCloud2>("lio_sam/feature/cloud_surface", 1);
+
+        floor_filtered_pub = nh.advertise<sensor_msgs::PointCloud2>("/floor_detection/floor_filtered_points", 32);
+        floor_points_pub = nh.advertise<sensor_msgs::PointCloud2>("/floor_detection/floor_points", 32);
+        floor_pub = nh.advertise<lio_sam::FloorCoeffs>("/floor_detection/floor_coeffs", 32);
         
         initializationValue();
     }
@@ -85,6 +106,27 @@ public:
         scan++;
         preTime = curTime;
 
+        //gky 地面点
+
+        auto detect_floor_start = ros::Time::now().toSec();
+        boost::optional<Eigen::Vector4f> floor = detect(extractedCloud);
+        auto detect_floor_end = ros::Time::now().toSec();
+        ROS_INFO_STREAM("\033[1;32m" << setw(22) << "detect floor takes" << (detect_floor_end - detect_floor_start) * 1000 << "ms\033[0m");
+        ROS_INFO_STREAM("\033[1;32m" << setw(22) << "FloorCoeffs: " << (*floor)[0] << (*floor)[1] << (*floor)[2] << (*floor)[3] << "\033[0m");
+        // publish the detected floor coefficients
+        lio_sam::FloorCoeffs coeffs;
+        coeffs.header = msgIn->header;
+        if(floor) {
+            coeffs.coeffs.resize(4);
+            for(int i = 0; i < 4; i++) {
+                coeffs.coeffs[i] = (*floor)[i];
+            }
+        }
+
+        floor_pub.publish(coeffs);
+
+
+        //**********
         calculateSmoothness();
 
         markOccludedPoints();
@@ -98,6 +140,154 @@ public:
         std::ofstream featureTaketime("/home/gky/桌面/slam_results/feature_take_time.csv", std::ios::app);
         featureTaketime << (featureTimeEnd - featureTimeStart) * 1000 <<endl;
     }
+
+    //地面点提取
+      /**
+   * @brief detect the floor plane from a point cloud
+   * @param cloud  input cloud
+   * @return detected floor plane coefficients
+   */
+    boost::optional<Eigen::Vector4f> detect(const pcl::PointCloud<PointType>::Ptr& cloud) const {
+    // compensate the tilt rotation 补偿倾斜
+    std::cout << "enter dector floor"<< std::endl;
+    Eigen::Matrix4f tilt_matrix = Eigen::Matrix4f::Identity();
+    //这里是绕y轴旋转
+    tilt_matrix.topLeftCorner(3, 3) = Eigen::AngleAxisf(0 * M_PI / 180.0f, Eigen::Vector3f::UnitY()).toRotationMatrix();
+
+    // filtering before RANSAC (height and normal filtering)
+    pcl::PointCloud<PointType>::Ptr filtered(new pcl::PointCloud<PointType>);
+    pcl::transformPointCloud(*cloud, *filtered, tilt_matrix);
+    filtered = plane_clip(filtered, Eigen::Vector4f(0.0f, 0.0f, 1.0f, 2.0 + 0.5), false);
+    filtered = plane_clip(filtered, Eigen::Vector4f(0.0f, 0.0f, 1.0f, 2.0 - 1.0), true);
+    ROS_INFO_STREAM("\033[1;37m" << setw(22) << "before filtered points :" << filtered->size() << "\033[0m");
+    //耗时较大接近100ms
+    if(1) {
+      filtered = normal_filtering(filtered);
+    }
+
+    pcl::transformPointCloud(*filtered, *filtered, static_cast<Eigen::Matrix4f>(tilt_matrix.inverse()));
+
+    if(floor_filtered_pub.getNumSubscribers()) {
+      filtered->header = cloud->header;
+      floor_filtered_pub.publish(*filtered);
+    }
+
+    // too few points for RANSAC
+    ROS_INFO_STREAM("\033[1;34m" << setw(22) << "filtered points :" << filtered->size() << "\033[0m");
+    if(filtered->size() < 512) {
+      return boost::none;
+    }
+
+    // RANSAC
+    pcl::SampleConsensusModelPlane<PointType>::Ptr model_p(new pcl::SampleConsensusModelPlane<PointType>(filtered));
+    pcl::RandomSampleConsensus<PointType> ransac(model_p);
+    ransac.setDistanceThreshold(0.1);
+    ransac.computeModel();
+
+    pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+    ransac.getInliers(inliers->indices);
+    
+    // too few inliers
+    ROS_INFO_STREAM("\033[1;35m" << setw(22) << "inliers points :" << filtered->size() << "\033[0m");
+    if(inliers->indices.size() < 512) {
+      return boost::none;
+    }
+
+    // verticality check of the detected floor's normal
+    Eigen::Vector4f reference = tilt_matrix.inverse() * Eigen::Vector4f::UnitZ();
+
+    Eigen::VectorXf coeffs;
+    ransac.getModelCoefficients(coeffs);
+
+    double dot = coeffs.head<3>().dot(reference.head<3>());
+    if(std::abs(dot) < std::cos(10 * M_PI / 180.0)) {
+      // the normal is not vertical
+      return boost::none;
+    }
+
+    // make the normal upward
+    if(coeffs.head<3>().dot(Eigen::Vector3f::UnitZ()) < 0.0f) {
+      coeffs *= -1.0f;
+    }
+
+    if(floor_points_pub.getNumSubscribers()) {
+      pcl::PointCloud<PointType>::Ptr inlier_cloud(new pcl::PointCloud<PointType>);
+      pcl::ExtractIndices<PointType> extract;
+      extract.setInputCloud(filtered);
+      extract.setIndices(inliers);
+      extract.filter(*inlier_cloud);
+      inlier_cloud->header = cloud->header;
+
+      floor_points_pub.publish(*inlier_cloud);
+    }
+
+    return Eigen::Vector4f(coeffs);
+  }
+//******************************************************************************************
+
+  /**
+   * @brief plane_clip
+   * @param src_cloud
+   * @param plane
+   * @param negative
+   * @return
+   */
+  pcl::PointCloud<PointType>::Ptr plane_clip(const pcl::PointCloud<PointType>::Ptr& src_cloud, const Eigen::Vector4f& plane, bool negative) const {
+    pcl::PlaneClipper3D<PointType> clipper(plane);
+    pcl::PointIndices::Ptr indices(new pcl::PointIndices);
+
+    clipper.clipPointCloud3D(*src_cloud, indices->indices);
+
+    pcl::PointCloud<PointType>::Ptr dst_cloud(new pcl::PointCloud<PointType>);
+
+    pcl::ExtractIndices<PointType> extract;
+    extract.setInputCloud(src_cloud);
+    extract.setIndices(indices);
+    extract.setNegative(negative);
+    extract.filter(*dst_cloud);
+
+    return dst_cloud;
+  }
+
+  /**
+   * @brief filter points with non-vertical normals
+   * @param cloud  input cloud
+   * @return filtered cloud
+   */
+  pcl::PointCloud<PointType>::Ptr normal_filtering(const pcl::PointCloud<PointType>::Ptr& cloud) const {
+    pcl::NormalEstimation<PointType, pcl::Normal> ne;
+    ne.setInputCloud(cloud);
+
+    pcl::search::KdTree<PointType>::Ptr tree(new pcl::search::KdTree<PointType>);
+    ne.setSearchMethod(tree);
+
+    pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
+    //搜索点附近的10个点计算法向量
+    ne.setKSearch(10);
+    ne.setViewPoint(0.0f, 0.0f, 1.5);
+    ne.compute(*normals);
+
+    pcl::PointCloud<PointType>::Ptr filtered(new pcl::PointCloud<PointType>);
+    filtered->reserve(cloud->size());
+
+    for(int i = 0; i < cloud->size(); i++) {
+        if(i == 0){
+            ROS_INFO_STREAM("\033[1;36m" << setw(22) << "first point normals :" << normals->at(i).getNormalVector3fMap().normalized() << "\033[0m");
+        }
+      float dot = normals->at(i).getNormalVector3fMap().normalized().dot(Eigen::Vector3f::UnitZ());
+      if(std::abs(dot) > std::cos(20 * M_PI / 180.0)) {
+        filtered->push_back(cloud->at(i));
+      }
+    }
+
+    filtered->width = filtered->size();
+    filtered->height = 1;
+    filtered->is_dense = false;
+
+    return filtered;
+  }
+
+
 
     void calculateSmoothness()
     {
